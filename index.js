5d4072b0cad9762e09754a31e3cc25343d2111b2
Neruu17 <145640712+Neruu17@users.noreply.github.com>
2023-09-21 20:57:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
+const BotName = 'LAW BOT'; 
+const instagram = 'https://instagram.com/zahcty_17'; 
 const telegram = 'https://t.me/reuzzz'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
